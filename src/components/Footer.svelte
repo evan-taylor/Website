7ea237c3dd3f7bb1970da12f884175963e41b574
Evan Taylor <eltaylor1104@gmail.com>
2025-01-30 00:46:51 -0800

    Fix HTML typo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="w-full py-6 text-center bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-300">
-  <a href="mailto:evan@evan-taylor.com"?><p>© {new Date().getFullYear()} Evan Taylor. All rights reserved.</p></a>
+  <a href="mailto:evan@evan-taylor.com"><p>© {new Date().getFullYear()} Evan Taylor. All rights reserved.</p></a>
 </footer>
