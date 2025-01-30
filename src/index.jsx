98a9164f960dc3fd1403859e8eaeeefc47f27523
Evan Taylor <eltaylor1104@gmail.com>
2025-01-29 22:48:57 -0800

    Fix import typo

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 import './index.css';  // Make sure this line is present
 
 const container = document.getElementById('root');
