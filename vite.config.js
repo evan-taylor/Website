0a95fcc312a11e9c10085ccd3ab9a9c81f0e3bab
Evan Taylor <eltaylor1104@gmail.com>
2025-01-30 00:10:29 -0800

    Remove dependency

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sveltekit } from '@sveltejs/kit/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    root: '.',
-    build: {
-        outDir: 'dist',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
-})
+/** @type {import('vite').UserConfig} */
+const config = {
+    plugins: [sveltekit()]
+};
+
+export default config;
