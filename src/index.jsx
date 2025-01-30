49a66bfcfb4c257ee13fd14acc76f05f183f0b5c
Evan Taylor <eltaylor1104@gmail.com>
2025-01-29 22:40:59 -0800

    Changes to CSS

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
-import './index.css';
+import App from './App';
+import './index.css';  // Make sure this line is present
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
