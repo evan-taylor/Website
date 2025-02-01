4946d968d9f020583bf6f895bf2cc6ac664c65cf
Evan Taylor <eltaylor1104@gmail.com>
2025-01-31 16:13:20 -0800

    Fix sitemap generation process

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,34 +1,34 @@
-import { base } from '$app/paths';
+export const prerender = true;
 
-export const prerender = true; 
+export async function GET({ url }) {
+  const base = url.origin; // Dynamically get the base URL from the request
 
-export async function GET() {
-    const pages = [
-        '', // Home page
-        'about',
-        'projects',
-        'contact',
-        'resume'
-    ];
+  const pages = [
+    '', // Home page
+    'about',
+    'projects',
+    'contact',
+    'resume'
+  ];
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages
-            .map((page) => {
-                return `
+      .map((page) => {
+        return `
       <url>
         <loc>${base}/${page}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <priority>${page === '' ? '1.0' : '0.8'}</priority>
       </url>
     `;
-            })
-            .join('')}
+      })
+      .join('')}
   </urlset>`;
 
-    return new Response(sitemap, {
-        headers: {
-            'Content-Type': 'application/xml'
-        }
-    });
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml'
+    }
+  });
 }
