ac94ab7a7cda9fc6012479a21489b05faabaa12f
Evan Taylor <eltaylor1104@gmail.com>
2025-03-09 20:31:46 -0700

    Change booking link

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -145,9 +145,21 @@
         data-aos-delay="200"
         class="bg-gray-800 rounded-xl border border-gray-700 p-4 md:p-6 shadow-xl opacity-0"
       >
-        <!-- Motion embed begin -->
-        <iframe src="https://app.usemotion.com/meet/evan-taylor/meet" title="Motion Booking Page" width="100%" height="840px" frameborder="0" class="rounded-lg"></iframe>
-        <!-- Motion embed end -->
+        <!-- Cal.com embed begin -->
+        <div style="width:100%;height:100%;overflow:auto" id="my-cal-inline"></div>
+        <script type="text/javascript">
+          (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
+          Cal("init", "meet", {origin:"https://cal.com"});
+
+          Cal.ns.meet("inline", {
+            elementOrSelector:"#my-cal-inline",
+            config: {"layout":"month_view"},
+            calLink: "evan-taylor/meet",
+          });
+
+          Cal.ns.meet("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+        </script>
+        <!-- Cal.com embed end -->
       </div>
     </div>
   {/if}
